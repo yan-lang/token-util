ae7a526cf4f045a9e42cc3af9d94148da5e93a4f
Zeqiang-Lai <916323301@qq.com>
2020-03-12 00:06:58 +0800

    feat: initial commit

---
+++ src/main/java/yan/Main.java
@@ -0,0 +1,10 @@
+package yan;
+
+import picocli.CommandLine;
+
+public class Main {
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new TokenUtil()).execute(args);
+        System.exit(exitCode);
+    }
+}
